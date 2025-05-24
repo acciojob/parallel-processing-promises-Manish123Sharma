ba126f560d1af6a428d49af2da29dde6ec6ea62c
Manish Kumar Sharma <39481751+Manish123Sharma@users.noreply.github.com>
2025-05-24 13:42:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+function downloadImage(url) {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = url;
+          img.onload = () => resolve(img);
+          img.onerror = () => reject(new Error(`Failed to load image: ${url}`));
+        });
+      }
+
+      function downloadImages(imageList) {
+        output.innerHTML = "";
+        errorDiv.innerText = "";
+        loadingDiv.style.display = "block";
+
+        const imagePromises = imageList.map(imgObj => downloadImage(imgObj.url));
+
+        Promise.all(imagePromises)
+          .then(images => {
+            loadingDiv.style.display = "none";
+            images.forEach(img => output.appendChild(img));
+          })
+          .catch(error => {
+            loadingDiv.style.display = "none";
+            errorDiv.innerText = error.message;
+          });
+      }
+
+      btn.addEventListener("click", () => {
+        downloadImages(images);
+      });
